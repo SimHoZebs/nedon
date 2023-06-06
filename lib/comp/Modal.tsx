f872dff32b1e877d588f1e6e81d9b3530e26591a
Sim Ho <simhozebs@gmail.com>
2023-06-05 20:55:19 -0400

    fix: modal closing when mouseup on backdrop

--- lib/comp/Modal.tsx
+++ lib/comp/Modal.tsx
@@ -8,14 +8,14 @@ const Modal = (props: Props) => {
   return (
     <div
       className="absolute left-0 top-0 z-10 flex h-screen w-screen items-center justify-center bg-zinc-900 bg-opacity-70 backdrop-blur-sm"
-      onClick={(e) => {
+      onMouseDown={(e) => {
         props.setShowModal(false);
         e.stopPropagation();
       }}
     >
       <div
         className="flex h-1/2 w-1/2 flex-col gap-y-3 bg-zinc-900 p-3 shadow-lg shadow-zinc-950"
-        onClick={(e) => {
+        onMouseDown={(e) => {
           e.stopPropagation();
         }}
       >
