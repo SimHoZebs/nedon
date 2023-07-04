783e02f48ce11b834c398ca614604eabe5fdefe2
Sim Ho <simhozebs@gmail.com>
2023-07-04 09:11:32 -0400

    fix PrimaryBtn props and colors

--- lib/comp/Button/PrimaryBtn.tsx
+++ lib/comp/Button/PrimaryBtn.tsx
@@ -4,7 +4,14 @@ import Button from ".";
 const PrimaryBtn = (props: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   const { children, ...rest } = props;
 
-  return <Button className="bg-blue-600 hover:bg-blue-700">{children}</Button>;
+  return (
+    <Button
+      className="bg-indigo-400 font-semibold text-zinc-800 hover:bg-indigo-500 hover:text-zinc-900"
+      {...rest}
+    >
+      {children}
+    </Button>
+  );
 };
 
 export default PrimaryBtn;
