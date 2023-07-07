391be30baa2a6db76f44c45f8e908d50f8552679
Sim Ho <simhozebs@gmail.com>
2023-07-06 22:36:06 -0400

    Improve Account choosing UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,9 @@
 import type { NextPage } from "next";
 import { trpc } from "../lib/util/trpc";
 import { useStoreActions, useStoreState } from "../lib/util/store";
-import NegativeBtn from "../lib/comp/Button/NegativeBtn";
-import addUserIcon from "../public/add-user.svg";
-import Image from "next/image";
 import { emptyUser } from "../lib/util/user";
-import deleteIcon from "../public/delete.svg";
 import { useRouter } from "next/router";
 import { Icon } from "@iconify-icon/react";
-import PrimaryBtn from "../lib/comp/Button/PrimaryBtn";
 import Button from "../lib/comp/Button";
 import { useEffect, useState } from "react";
 
@@ -39,45 +34,48 @@ const Home: NextPage = () => {
 
   return (
     <section className="flex h-full w-full flex-col items-center justify-center gap-y-3">
-      {allUsers.isLoading && <h1 className="text-3xl">Loading...</h1>}
+      <h1 className="text-3xl">
+        {allUsers.isLoading ? "Loading" : "Choose an account"}
+      </h1>
 
       {allUsers.data && (
-        <>
-          <h1 className="text-3xl">Choose an account</h1>
+        <div className="flex flex-col items-center rounded-md border border-zinc-600 ">
+          {allUsers.data.map((user) => (
+            <div
+              key={user.id}
+              className="flex w-full items-center justify-between border-b border-zinc-600 p-3 hover:cursor-pointer sm:gap-x-16"
+              onClick={async (e) => {
+                // const isPaymentInit = user.products.includes(
+                //   Products.PaymentInitiation
+                // );
+                // setIsPaymentInitiation(isPaymentInit);
 
-          <div className="flex flex-col items-center rounded-md border border-zinc-600 sm:w-2/3">
-            {allUsers.data.map((user) => (
-              <div
-                key={user.id}
-                className="flex w-full justify-between gap-y-2 border-b border-zinc-600 p-3 hover:cursor-pointer"
-                onClick={async (e) => {
-                  // const isPaymentInit = user.products.includes(
-                  //   Products.PaymentInitiation
-                  // );
-                  // setIsPaymentInitiation(isPaymentInit);
+                setAppUser((prev) => user);
 
-                  setAppUser((prev) => user);
+                if (!user.groupArray) return;
+                const group = await server.group.get.fetch({
+                  id: user.groupArray[0].id,
+                });
 
-                  if (!user.groupArray) return;
-                  const group = await server.group.get.fetch({
-                    id: user.groupArray[0].id,
-                  });
-
-                  if (!group) return;
-                  setAppGroup((prev) => group);
-                  router.push("/home");
-                }}
-              >
-                <div className="flex flex-col">
-                  <p>userId: {user.id}</p>
-                  <p>
-                    hasAccessToken: {user.hasAccessToken ? "true" : "false"}
-                  </p>
-                </div>
-
-                <div className="flex h-full min-w-fit items-center">
-                  {appUser && appUser.id !== user.id && appGroup && (
-                    <PrimaryBtn
+                if (!group) return;
+                setAppGroup((prev) => group);
+                router.push("/home");
+              }}
+            >
+              <div className="flex gap-x-2">
+                <p>userId: {user.id.slice(0, 8)}</p>
+              </div>
+              <div className="flex h-full min-w-fit items-center gap-x-1">
+                {appUser && appUser.id !== user.id && appGroup && (
+                  <>
+                    <Button
+                      className={`after:h-full after:w-px after:bg-zinc-500 ${
+                        appGroup.userArray?.find(
+                          (groupUser) => groupUser.id === user.id
+                        )
+                          ? "text-pink-300"
+                          : "text-blue-300"
+                      }`}
                       onClick={async (e) => {
                         e.stopPropagation();
 
@@ -99,41 +97,37 @@ const Home: NextPage = () => {
                       {appGroup.userArray?.find(
                         (groupUser) => groupUser.id === user.id
                       ) ? (
-                        <Icon icon={"mdi:delete-outline"} width={24} />
+                        <Icon icon="mdi:user-remove-outline" width={20} />
                       ) : (
-                        <Image
-                          src={addUserIcon}
-                          height={24}
-                          width={24}
-                          alt=""
-                        />
+                        <Icon icon="mdi:user-add-outline" width={20} />
                       )}
-                    </PrimaryBtn>
-                  )}
-
-                  <NegativeBtn
-                    onClick={async (e) => {
-                      e.stopPropagation();
-                      if (!user.groupArray) return;
-                      await deleteGroup.mutateAsync({
-                        id: user.groupArray[0].id,
-                      });
-                      await deleteUser.mutateAsync(user.id);
-                      allUsers.refetch();
-                      if (appUser && appUser.id === user.id) {
-                        setAppUser(() => emptyUser);
-                      }
-                    }}
-                  >
-                    <Image src={deleteIcon} height={16} alt="" />
-                  </NegativeBtn>
-                </div>
+                    </Button>
+                    <div className="flex h-full w-px bg-zinc-500"></div>
+                  </>
+                )}
+                <Button
+                  className="text-pink-300"
+                  onClick={async (e) => {
+                    e.stopPropagation();
+                    if (!user.groupArray) return;
+                    await deleteGroup.mutateAsync({
+                      id: user.groupArray[0].id,
+                    });
+                    await deleteUser.mutateAsync(user.id);
+                    allUsers.refetch();
+                    if (appUser && appUser.id === user.id) {
+                      setAppUser(() => emptyUser);
+                    }
+                  }}
+                >
+                  <Icon icon="mdi:delete-outline" width={20} />
+                </Button>
               </div>
-            ))}
+            </div>
+          ))}
 
-            <CreateUserBtn userIdArray={userIdArray} />
-          </div>
-        </>
+          <CreateUserBtn userIdArray={userIdArray} />
+        </div>
       )}
     </section>
   );
