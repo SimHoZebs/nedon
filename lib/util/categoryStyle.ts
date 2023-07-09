6bc906841d57d1de8d9e348309d09afb513dc699
Sim Ho <simhozebs@gmail.com>
2023-07-09 01:01:50 -0400

    Add more icons

--- lib/util/categoryStyle.ts
+++ lib/util/categoryStyle.ts
@@ -3,16 +3,16 @@ const categoryStyle: {
 } = {
   Travel: { icon: "mdi:car-outline", color: "bg-yellow-300" },
   Taxi: { icon: "bx:taxi", color: "bg-yellow-300" },
-  "Fast Food": { icon: "mdi:food", color: "bg-red-300" },
+  "Fast Food": { icon: "majesticons:burger-line", color: "bg-red-300" },
   "Food and Drink": {
     icon: "fluent:food-24-regular",
     color: "bg-blue-300",
   },
   "Airlines and Aviation Services": {
-    icon: "mdi:airplane",
+    icon: "lucide:plane-takeoff",
     color: "bg-teal-300",
   },
-  "Coffee Shop": { icon: "mdi:coffee", color: "bg-orange-300" },
+  "Coffee Shop": { icon: "ci:coffee", color: "bg-orange-300" },
   Shops: { icon: "mdi:shopping-outline", color: "bg-green-300" },
   Payment: { icon: "mdi:exchange", color: "bg-green-300" },
   Transfer: { icon: "mdi:exchange", color: "bg-green-300" },
@@ -23,6 +23,30 @@ const categoryStyle: {
   Recreation: {
     icon: "material-symbols:relax-outline",
     color: "bg-green-300",
+  },
+  Overdraft: {
+    icon: "ph:hand-coins-bold",
+    color: "bg-red-300",
+  },
+  Rent: {
+    icon: "tabler:home-dollar",
+    color: "bg-green-300",
+  },
+  Deposit: {
+    icon: "mdi:instant-deposit",
+    color: "bg-zinc-300",
+  },
+  Restaurants: {
+    icon: "ri:restaurant-line",
+    color: "bg-yellow-300",
+  },
+  "Credit Card": {
+    icon: "mdi:credit-card-outline",
+    color: "bg-zinc-300",
+  },
+  Credit: {
+    icon: "mdi:bank-outline",
+    color: "bg-orange-300",
   },
 };
 
