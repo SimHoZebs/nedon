296a95faabb0f788457a236b277f5bdce296bbb9
Sim Ho <simhozebs@gmail.com>
2024-03-14 22:36:07 -0400

    Add: display connections in connections page

--- pages/connections.tsx
+++ pages/connections.tsx
@@ -1,23 +1,33 @@
-import router from "next/router";
 import React, { useMemo, useState } from "react";
 
-import { ActionBtn } from "@/comp/Button";
+import { ActionBtn, Button } from "@/comp/Button";
 import SettleModal from "@/comp/analysis/SettleModal";
 
 import getAppUser from "@/util/getAppUser";
 import parseMoney from "@/util/parseMoney";
 import { trpc } from "@/util/trpc";
 
-const Splits = () => {
+const Connections = () => {
   const [showModal, setShowModal] = useState(false);
   const [oweUser, setOweUser] = useState<{ id: string; amount: number }>();
 
   const { appUser } = getAppUser();
 
+  const appGroup = trpc.group.get.useQuery(
+    { id: appUser?.groupArray?.[0].id || "" },
+    { staleTime: Number.POSITIVE_INFINITY, enabled: !!appUser },
+  );
+
+  const connectionsArray = appGroup.data?.userArray?.filter(
+    (user) => user.id !== appUser?.id,
+  );
+
+  const removeUserFromGroup = trpc.group.removeUser.useMutation();
   const associatedTxArray = trpc.tx.getAllAssociated.useQuery(
     { id: appUser ? appUser.id : "" },
     { staleTime: 3600000, enabled: !!appUser },
   );
+  const queryClient = trpc.useUtils();
 
   const calcOweGroup = useMemo(() => {
     if (!associatedTxArray.data) {
@@ -30,12 +40,12 @@ const Splits = () => {
     }
     const oweGroup: { [userId: string]: number } = {};
 
-    for (const tx of associatedTxArray.data) {
-      if (!appUser) {
-        console.error("appUser not found");
-        return;
-      }
+    if (!appUser) {
+      console.error("appUser not found");
+      return;
+    }
 
+    for (const tx of associatedTxArray.data) {
       for (const split of tx.splitArray) {
         const splitAmount = split.catArray.reduce(
           (total, cat) => total + cat.amount,
@@ -43,7 +53,7 @@ const Splits = () => {
         );
 
         if (tx.ownerId === appUser.id) {
-          if (split.userId === appUser.id) return;
+          if (split.userId === appUser.id) continue;
 
           //amount others owe appUser
           oweGroup[split.userId] = oweGroup[split.userId]
@@ -69,49 +79,61 @@ const Splits = () => {
         <SettleModal oweUser={oweUser} setShowModal={setShowModal} />
       )}
 
-      <div className="sm:w-full">
-        <ActionBtn onClick={() => router.push("/user")}>
-          <div className="flex items-center gap-x-2">
-            <span className="icon-[mdi--user-add-outline] h-6 w-6" />
-            <p>Add friend</p>
-          </div>
-        </ActionBtn>
-      </div>
-
-      <div className="flex flex-col items-start">
-        {calcOweGroup &&
-          Object.keys(calcOweGroup).map((userId) => (
-            <div key={userId} className="flex items-center gap-x-2">
-              <div className="flex items-center gap-x-3 rounded-lg bg-zinc-800 px-3 py-2">
-                <div className="flex flex-col items-center gap-1">
-                  <div className="flex rounded-full border border-zinc-700 p-1">
-                    <span className="icon-[mdi--account] h-6 w-6" />
-                  </div>
-                  <div className="text-xs text-zinc-400">
-                    {userId.slice(0, 8)}
-                  </div>
-                </div>
-                <div>
-                  {calcOweGroup[userId] < 0 ? "You owe: " : "They owe: "}$
-                  {Math.abs(parseMoney(calcOweGroup[userId]))}
-                </div>
+      <div className="flex max-w-xl flex-col  gap-4">
+        {connectionsArray?.map((user) => (
+          <div
+            className="flex h-fit w-full items-center justify-between gap-x-4 gap-y-1 rounded-xl bg-zinc-800 px-3 py-2 text-start outline outline-1 outline-zinc-700"
+            key={user.id}
+          >
+            <div className="flex flex-col items-center justify-center">
+              <div className="flex rounded-full border p-2">
+                <span className="icon-[mdi--account] h-8 w-8 " />
               </div>
-              <ActionBtn
-                onClick={() => {
-                  setShowModal(true);
-                  setOweUser({
-                    id: userId,
-                    amount: parseMoney(calcOweGroup[userId] * 100),
-                  });
-                }}
-              >
-                Manually settle
-              </ActionBtn>
+              <p>{user.name}</p>
             </div>
-          ))}
+            {calcOweGroup?.[user.id] && (
+              <div className="flex flex-col items-start gap-2">
+                <p>
+                  {`${calcOweGroup[user.id] < 0 ? "You" : "They"} owe: $${Math.abs(parseMoney(calcOweGroup[user.id]))}`}
+                </p>
+
+                <ActionBtn
+                  onClick={() => {
+                    setShowModal(true);
+                    setOweUser({
+                      id: user.id,
+                      amount: parseMoney(calcOweGroup?.[user.id] || 0 * 100),
+                    });
+                  }}
+                >
+                  <span className="icon-[mdi--cash] h-5 w-5" />
+                  Manually settle
+                </ActionBtn>
+              </div>
+            )}
+
+            <Button
+              className="text-pink-400 after:h-full after:w-px after:bg-zinc-500"
+              onClickAsync={async (e) => {
+                if (!appGroup.data) return;
+                e.stopPropagation();
+
+                await removeUserFromGroup.mutateAsync({
+                  groupId: appGroup.data.id,
+                  userId: user.id,
+                });
+
+                await queryClient.user.getAll.invalidate();
+                await queryClient.group.get.invalidate();
+              }}
+            >
+              <span className="icon-[mdi--user-remove-outline] h-5 w-5" />
+            </Button>
+          </div>
+        ))}
       </div>
     </section>
   );
 };
 
-export default Splits;
+export default Connections;
