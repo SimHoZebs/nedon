cfd756681680f6a9816826b687d9bc1e99b8380b
SimHoZebs <simhozebs@gmail.com>
2024-10-30 23:22:25 -0400

    remove csv upload from settings

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,7 +1,5 @@
 import { useRouter } from "next/router";
-import Papa from "papaparse";
-import React, { type ChangeEvent, useRef } from "react";
-import { z } from "zod";
+import React from "react";
 
 import { ActionBtn, Button } from "@/comp/Button";
 import { H2 } from "@/comp/Heading";
@@ -10,58 +8,19 @@ import CreateUserBtn from "@/comp/user/CreateuserBtn";
 import getAppUser from "@/util/getAppUser";
 import { useLocalStore, useStore } from "@/util/store";
 import { trpc } from "@/util/trpc";
-import { createTxFromChaseCSV } from "@/util/tx";
-
-import { type ChaseCSVTx, ChaseCSVTxSchema } from "@/types/tx";
 
 const Settings = () => {
   const router = useRouter();
   const verticalCatPicker = useStore((state) => state.verticalCatPicker);
   const setVerticalCatPicker = useStore((state) => state.setVerticalCatPicker);
-  const [csvTxArray, setCsvTxArray] = React.useState<ChaseCSVTx[]>([]);
-  const csvInputRef = useRef<HTMLInputElement>(null);
 
   const { appUser, allUsers } = getAppUser();
   const deleteAll = trpc.user.deleteAll.useMutation();
   const deleteUser = trpc.user.delete.useMutation();
   const addConnection = trpc.user.addConnection.useMutation();
   const removeConnection = trpc.user.removeConnection.useMutation();
-  const createManyTx = trpc.tx.createMany.useMutation();
   const queryClient = trpc.useUtils();
   const setUserId = useLocalStore((state) => state.setUserId);
-
-  const csvToTx = async (e: ChangeEvent<HTMLInputElement>) => {
-    console.log("csvToTx");
-    const file = e.target.files?.[0];
-    if (!file) return;
-    const reader = new FileReader();
-
-    reader.onload = async (e) => {
-      const text = e.target?.result;
-      if (!text) return;
-
-      if (typeof text === "string") {
-        const { data } = Papa.parse(text, {
-          header: true,
-          skipEmptyLines: true,
-          transformHeader: (header) => header.replace(/[^a-zA-Z]/g, ""),
-        });
-
-        console.log("data", data);
-
-        const chaseTxArray = z.array(ChaseCSVTxSchema).safeParse(data);
-        if (!chaseTxArray.success) {
-          console.error(chaseTxArray.error);
-          return;
-        }
-
-        setCsvTxArray(chaseTxArray.data);
-        console.log(chaseTxArray.data);
-      }
-    };
-
-    reader.readAsText(file);
-  };
 
   return (
     <section className="flex h-full w-full flex-col items-start gap-y-3">
@@ -87,32 +46,6 @@ const Settings = () => {
         />
         <label htmlFor="verticalCatPicker">make cat picker vertical</label>
       </div>
-
-      <input type="file" onChange={csvToTx} ref={csvInputRef} />
-      <Button
-        onClick={() => {
-          const el = csvInputRef.current;
-          if (!el) return;
-          el.value = "";
-        }}
-      >
-        reset csvToTx
-      </Button>
-      <Button
-        onClickAsync={async () => {
-          if (!appUser || csvTxArray.length < 1) return;
-
-          const txArray = csvTxArray.map((csvTx) =>
-            createTxFromChaseCSV(csvTx, appUser.id),
-          );
-          console.log("uploading txArray");
-          await createManyTx.mutateAsync(txArray);
-          await queryClient.tx.getAll.invalidate();
-          console.log("done");
-        }}
-      >
-        upload csvTx
-      </Button>
 
       <H2>DEBUG AREA</H2>
       <div className="flex flex-col gap-3">
